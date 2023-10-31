f009339c7b5b678809b99c7765d9b18706b38df8
char502 <char502@hotmail.com>
2023-10-31 22:55:06 +0000

    no longer requiring resend api key

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 // Need to set up a domain to get this to work
 
